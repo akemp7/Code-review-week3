d7aa7b7c0196e39bb1e403cc5cc8bc972fe4780a
Anita Kemp <anita.k.kemp@gmail.com>
2019-08-23 16:03:06 -0700

    Refactor code further

--- js/scripts.js
+++ js/scripts.js
@@ -13,22 +13,21 @@ function numberRange (numberInput) {
   var output= ''
   for(var i=0; i<=numberInput; i++){
     newRange.push(i);
-    var stringRange= newRange.toString();
+    var digits= newRange.toString();
   }
-  var digits = stringRange;
   if (numberInput>100){
     alert("Please enter a number less than 100");
   } else {
     if (digits.match(/[3]/) ) {
-      var digitThree = digits.replace(/[0-9]?[3].?/g, "I'm sorry, Dave. But I'm afraid can't do that.");
+      var digitThree = digits.replace(/[0-9]?[3+].?/g, "I'm sorry, Dave. But I'm afraid can't do that.");
       output = digitThree;
 
   } if (digits.match(/[2]/)) {
-    var digitTwo = digitThree.replace(/[0-9]?[2].?/g, "boop");
+    var digitTwo = digitThree.replace(/[0-9]?[2+].?/g, "boop");
     output = digitTwo;
 
   }  if (digits.match(/[1]/)){
-    var digitOne = digitTwo.replace(/[0-9]?[1].?/g, "beep");
+    var digitOne = digitTwo.replace(/[0-9]?[1+].?/g, "beep");
     output = digitOne;
   }
   return output;
