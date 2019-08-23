cb97a2c3008562b712211ffd526bfb95c164460d
Anita Kemp <anita.k.kemp@gmail.com>
2019-08-23 14:06:13 -0700

    Refactor code to delete if statements for single digits

--- js/scripts.js
+++ js/scripts.js
@@ -17,37 +17,21 @@ function numberRange (numberInput) {
     var stringRange= newRange.toString();
   }
 
-//takes care of single digits in a range of 0-9//
-  if (stringRange.match('1')){
-    var toBeep = stringRange.replace('1', 'beep');
 
-    if (toBeep.match('2')) {
-      var toBoop = toBeep.replace('2', 'boop');
-
-      if (toBoop.match('3')){
-      var  hal = toBoop.replace('3', "I'm sorry, Dave. But I'm afraid can't do that.")
-
-        }
-      }
-    }
-
-  //takes care of double digits//
-  var doubleDigits = hal;
-  if (numberInput>100){
-    alert("Please enter a number less than 100");
-  } else if (doubleDigits.match(/[0-9][3]/g|/[3][0-9]/g) ) {
-      var digitThree = doubleDigits.replace(/[0-9][3]/g, "I'm sorry, Dave. But I'm afraid can't do that.");
-       digitThree = digitThree.replace(/[3][0-9]/g, "I'm sorry, Dave. But I'm afraid can't do that.");
+  var doubleDigits = stringRange;
+  // if (numberInput>100){
+  //   alert("Please enter a number less than 100");
+  // }
+   if (doubleDigits.match(/[3]/) ) {
+      var digitThree = doubleDigits.replace(/[0-9]?[3].?/g, "I'm sorry, Dave. But I'm afraid can't do that.");
       output = digitThree;
 
-  } if (doubleDigits.match(/[0-9][2]/g|/[2][0-9]/g)) {
-    var digitTwo = digitThree.replace(/[0-9][2]/g, "boop");
-     digitTwo = digitTwo.replace(/[2][0-9]/g, "boop");
+  } if (doubleDigits.match(/[2]/)) {
+    var digitTwo = digitThree.replace(/[0-9]?[2].?/g, "boop");
     output = digitTwo;
 
-  }  if (doubleDigits.match(/[0-9][1]/g|/[1][0-9]/g)){
-    var digitOne = digitTwo.replace(/[1][0-9]/g, "beep");
-    digitOne= digitOne.replace(/[0-9][1]/g, "beep");
+  }  if (doubleDigits.match(/[1]/)){
+    var digitOne = digitTwo.replace(/[0-9]?[1].?/g, "beep");
     output = digitOne;
   }
 
