df850a651504dd46b0a12ca345cf90ffa96e9aa0
Anita Kemp <anita.k.kemp@gmail.com>
2019-08-23 10:07:18 -0700

    replace the single digit 2 and 3 with words

--- js/scripts.js
+++ js/scripts.js
@@ -10,15 +10,25 @@ $(document).ready(function(){
 
 ////Business Logic////
 function numberRange (numberInput) {
-  var newRange = [];
+  var newRange = [];;
+  var test = ''
+  var output= ''
   for(var i=0; i<=numberInput; i++){
     newRange.push(i);
-    var test= newRange.toString();
-    var newTest = test.replace('1', 'beep');
-    console.log(newTest);
+    var stringRange= newRange.toString();
+    if (stringRange.match(/[1]/)){
+      output = stringRange.replace(/[1]/, 'beep');
+    }
+
+    if (stringRange.match(/[2]/)) {
+       output = stringRange.replace(/[2]/, 'boop');
+    }
+
+    if (stringRange.match(/[3]/)) {
+       output = stringRange.replace(/[3]/, "I'm sorry Dave, but I'm afraid I cannot do that");
+    }
+
+
+      console.log(output);
   }
-
-
-
-
 }
