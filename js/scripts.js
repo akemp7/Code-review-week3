b899a6870bdf9be23a8a1696e7d2a6c64248e7eb
Anita Kemp <anita.k.kemp@gmail.com>
2019-08-23 11:13:42 -0700

    Replace all digits containing 3 e.g. 13, 33, with Hal phrase

--- js/scripts.js
+++ js/scripts.js
@@ -16,32 +16,27 @@ function numberRange (numberInput) {
   for(var i=0; i<=numberInput; i++){
     newRange.push(i);
     var stringRange= newRange.toString();
+  }
 
-    if (stringRange.match('1')){
-      var beep = stringRange.replace('1', 'beep');
+//takes care of single digits in a range of 0-9//
+  if (stringRange.match('1')){
+    var beep = stringRange.replace('1', 'beep');
 
-      if (beep.match('2')) {
+    if (beep.match('2')) {
       var boop = beep.replace('2', 'boop');
 
-        if (boop.match('3')){
-        output = boop.replace('3', "Im sorry Dave, but I can't do that.")
-        console.log(output);
+      if (boop.match('3')){
+      var  double = boop.replace('3', "Im sorry Dave, but I can't do that.")
+
         }
       }
-
     }
-    // if (boop.match('3')){
-    //   output = boop.replace('3', "I'm sorry Dave, but I can't do that.")
-    //   console.log(output);
-    // }
 
-
-
-
-    // if (stringRange.match(/[3]/)) {
-    //    output = stringRange.replace(/[3]/, "I'm sorry Dave, but I'm afraid I can't do that");
-    // }
-
-
+  //takes care of double digits//
+  var doubleDigits = double;
+  if (doubleDigits.match(/[0-9][3]/g || /[3][0-9]/g) ) {
+    var test = doubleDigits.replace(/[0-9][3]/g, "I'm sorry Dave, but I can't do that.");
+    output = test.replace(/[3][0-9]/g, "I'm sorry Dave, but I can't do that.");
+    return output;
   }
 }
